79bf5236af26ce00a9619005079a9daccd0b532e
Chance Mims <chance@Chances-MacBook-Air.local>
2020-01-17 12:01:42 -0600

    Code challenge 2

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,36 @@
 class HeroinesController < ApplicationController
+  before_action :set_heroine, only: [:show]
+
+
   def index
     @heroines = Heroine.all
   end
+
+  def show
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params(:super_name, :name, :power_id))
+    if @heroine.valid?
+      @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params(*args)
+    params.require(:heroine).permit(*args)
+  end
+
 end
